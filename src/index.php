f274822c08d66203aa6cc375e5c8ec2f3394e6cb
YukiTakada <yuki.takada.3112@gmail.com>
2022-01-07 21:53:55 +0900

    一時コミット

--- src/index.php
+++ src/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+require_once('Assert.php');
+
 /**
  * @param array $event
  * @return void
@@ -12,4 +14,11 @@ function index(array $event): void
 
     /** @var array $records */
     $records = $event['Records'];
+
+    foreach ($records as $record) {
+        // オブジェクト名, バケット名取得
+        // S3クライアント作成
+        // コピー実行
+        // ログ出力
+    }
 }
