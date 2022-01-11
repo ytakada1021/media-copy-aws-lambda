881763a5982147c17f2d3f74ca9a8741d51f6670
YukiTakada <yuki.takada.3112@gmail.com>
2022-01-11 13:57:22 +0900

    index.phpを実装

--- src/index.php
+++ src/index.php
@@ -4,21 +4,53 @@ declare(strict_types=1);
 
 require_once('Assert.php');
 
+use Aws\S3\S3Client;
+
 /**
  * @param array $event
  * @return void
  */
 function index(array $event): void
 {
-    Assert::notNull($event, '$event cannot be null.');
+    Assert::notEmpty($_ENV['DESTINATION_S3_BUCKET_NAME'],  '$_ENV["DESTINATION_S3_BUCKET_NAME"] must be provided.');
 
     /** @var array $records */
     $records = $event['Records'];
 
     foreach ($records as $record) {
-        // オブジェクト名, バケット名取得
-        // S3クライアント作成
-        // コピー実行
-        // ログ出力
+        $message = json_decode($record['Sns']['Message'], true);
+
+        /** @var string */
+        $sourceBucketName = $message['Records'][0]['s3']['bucket']['name'];
+
+        /** @var string */
+        $sourceObjectKey = $message['Records'][0]['s3']['object']['key'];
+
+        $s3Client = new S3Client([
+            'version' => 'latest',
+            'region'  => 'ap-northeast-1'
+        ]);
+
+        $destinationBucketName = $_ENV['DESTINATION_S3_BUCKET_NAME'];
+
+        $destinationObjectKey = $sourceObjectKey;
+
+        Assert::isTrue($sourceBucketName !== $destinationBucketName, '$sourceBucketName and $destinationBucketName must be different');
+
+        $s3Client->copyObject([
+            'Bucket'     => $destinationBucketName,
+            'Key'        => "{$destinationObjectKey}",
+            'CopySource' => "{$sourceBucketName}/{$sourceObjectKey}",
+        ]);
+
+        print(<<< EOT
+        Successfully copied s3 object.
+        Source:
+            Bucket Name: $sourceBucketName
+            Object Key: $sourceObjectKey
+        Destination:
+            Bucket Name: $destinationBucketName
+            Object Key: $destinationObjectKey\n
+        EOT);
     }
 }
